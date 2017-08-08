c05a8730f3ad8fb106c76bd5f303126d60184362
Jevilla <juan.es-19@hotmail.com>
2017-08-07 20:04:28 -0500

    partial program without use bind, task #9

---
+++ arguments.js
@@ -0,0 +1,24 @@
+/**
+ *  to pass arguments to a function in javascript there is no need to 
+ * say it to function how many variables will be accepted, 
+ * you can know what arguments are being passed to function using
+ * ** arguments **, this is an object 
+ */
+
+function dummy() {
+	console.log( Array.prototype.slice.call(arguments))
+}
+dummy(1, 2, 3, "asd", "kjuan", 30, [5,6,7]);
+
+
+
+
+
+function dummy2(a) {
+	return function(b){
+		console.log(a, b)
+	}
+}
+
+var invq = dummy2('hello');
+invq("fsd");
